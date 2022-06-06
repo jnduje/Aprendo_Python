8bacbb355b99ec0f35204807afa555a899fd0a44
Nicolas Duje <nicoduje@gmail.com>
2022-06-06 19:32:07 -0300

    Termino ejercico del ahorcado

--- Python_Intermedio/curso-python/juego_del_ahorcado.py
+++ Python_Intermedio/curso-python/juego_del_ahorcado.py
@@ -1,21 +1,22 @@
 import random
+import os
 
 def read_word():
     list_words = []
     with open ('./archivos/data.txt', 'r', encoding= 'utf-8') as f:
         for word in f:
             word = word.strip('\n')
+            word = word.upper()
             list_words.append(word)
-         
     return random.choice(list_words)
 
+# def secret_word_to_list(word):
+#     secret_list = []
+#     for c in word:
+#         secret_list = secret_list.append(c)
+#     return secret_list
 
-def word_to_list(word):
-    result = word.upper()
-    
 
-    
-    
 
 # def print_result (word, caracter):
 #     result = ""
@@ -37,15 +38,38 @@ def word_to_list(word):
 #     print(result)
     
     return result
-    
 
 
 def run():
-    caracter = input("Ingrese un caracter: ")
+    
     secret_word = read_word()
-    print(secret_word)
-    print(word_to_list(secret_word))
-   # print_result(secret_word, caracter)
+    chosen_word_list = [letter for letter in secret_word]
+    chosen_word_list_underscores = ["_"] * len(chosen_word_list)
+    
+    #print(secret_word)
+    #input("Ayuda para crear el programa")
+
+    while True:
+        os.system("cls") # Si estás en Unix (Mac o Linux) cambia cls por clear
+        print("¡Adivina la palabra!")
+        for element in chosen_word_list_underscores:
+            print(element + " ", end="")
+        print("\n")
+
+        caracter = input("Ingrese una letra: ")
+        assert caracter.isalpha(), "Solo puedes ingresar letras"
+
+        count = 0
+        for c in chosen_word_list:
+            if caracter.upper() == c:
+                chosen_word_list_underscores[count] = c
+            count += 1
+        
+        if "_" not in chosen_word_list_underscores:
+            os.system("cls") # Si estás en Unix (Mac o Linux) cambia cls por clear
+            print("¡Ganaste! La palabra era", secret_word)
+            break
+    
 
 if __name__ == '__main__':
     run()
