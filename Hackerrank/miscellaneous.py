c6dadc3c4a191ced4ea068b6f50a9b77638742b8
Nico_PG6 <nicoduje@gmail.com>
2022-06-13 05:27:36 -0300

    Ejercicio list

--- Hackerrank/miscellaneous.py
+++ Hackerrank/miscellaneous.py
@@ -1,12 +1,35 @@
 def run():
-    N = int(input())
+    N = int(input("Ingrese la cantidad de comandos: "))
     list = []
-
+      
     for i in range(N):
-        index, value = input().split()
+        print(f"comando N°: {i + 1}")
+        comand, *rest = input().split()
 
-        list.insert(int(index), int(value))
-    print(list)
 
+        # try:
+        #     index = int(rest[0])
+        #     integer = int(rest[1])
+            
+        # except IndexError:
+        #     integer = int(rest[0]) 
+
+        if comand == 'insert':
+            list.insert(index, integer)
+        elif comand == 'print':
+            print(list)
+        elif comand == 'remove':
+            list.remove(integer)
+        elif comand == 'append':
+            list.append(integer)
+        elif comand == 'sort':
+            list.sort()
+        elif comand == 'pop':
+            list.pop()
+        elif comand == 'reverse':
+            list.reverse()
+        
+
+    
 if __name__ == '__main__':
     run()
