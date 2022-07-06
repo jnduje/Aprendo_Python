2c22f17811fde3930d2491741e4da08f708ddc0b
Nicolas Duje <nicoduje@gmail.com>
2022-07-05 21:13:16 -0300

    Comprimo primer archivo pero en gz

--- Ejercicios_Bruno/ejercicio-1/ejercicio1.py
+++ Ejercicios_Bruno/ejercicio-1/ejercicio1.py
@@ -3,6 +3,9 @@ from PIL import Image
 from datetime import datetime, date, time, timezone
 from pytz import utc
 import gzip
+import shutil
+import os
+
 
 
 
@@ -25,21 +28,22 @@ def run(playwright: Playwright) -> None:
     # Trabajo con las fechas
     datetime_print = datetime.now(tz=timezone.utc)
     name_print = datetime_print.strftime("%d%m%Y_%H%M%S")
+    backup_name = datetime_print.strftime("%d%m%Y_%H")
     
     # Abro imagen, convierto a escala de grises y luego la guardo con otro nombre
     img = Image.open('firstprint.jpg')
     imgGray = img.convert('L')
     imgGray.save('IMG_' + name_print + '.jpg')
 
+    #----------------------------------------------------   
+
+    with open('IMG_' + name_print + '.jpg', 'rb') as f_in:
+        with gzip.open('backup_' + backup_name + ".jpg.gz", "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
     
 
-
-
-
+   # compress(backup_name, data)
     
-
-
-
 with sync_playwright() as playwright:
     run(playwright)
 
