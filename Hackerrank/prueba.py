331528e7a62925d96eecac0cf853232f994576b2
Nico Duje <nicoduje@gmail.com>
2022-07-25 08:17:09 -0300

    Formateo str desde WSL

--- Hackerrank/prueba.py
+++ Hackerrank/prueba.py
@@ -1,28 +1,24 @@
 
-def header(rows, columns):
-    text = '.|.'
-   
-    for i in range(rows):
-        j = 1 + (2*i)
-        print_text = text * j
-        print (print_text.center(columns,'-'))
-  
-def center(columns):
-    print ('WELCOME'.center(columns,'-'))
-    
-def footer(rows, columns):
-    text = '.|.'
-    
-    for i in range(rows):
-        j = (rows*2) - (1 + (2*i))
-        print_text = text * j
-        print (print_text.center(columns,'-'))
-        
-    
+def print_formatted(n):
+
+    row_with = len(bin(n)[2:])
+
+    for i in range(0, n):
+        i += 1
+        str_dec = str(i)
+        str_oct = str(oct(i))
+        str_hex = hex(i)
+        str_bin = bin(i)
+       # print(f'{i} {str_oct[2:].rjust(row_with)} {str_hex[2:].upper().rjust(row_with)} {str_bin[2:].rjust(row_with)}')
+        print(f'{str_dec.rjust(row_with)}', end = " ")
+        print(f'{str_oct[2:].rjust(row_with)}', end = " ")
+        print(f'{str_hex[2:].upper().rjust(row_with)}', end = " ")
+        print(f'{str_bin[2:].rjust(row_with)}')
+
 
 if __name__ == '__main__':
-
-    num = int(input())
+    n = int(input())
+    print_formatted(n)
 
     # Octal oct(num)
     # Binario bin(num)
