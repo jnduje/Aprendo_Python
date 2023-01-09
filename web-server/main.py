948b8f5bc02e0c73640cf9c91f0e563dfac34fb4
Nico Duje <nicoduje@gmail.com>
2023-01-08 22:58:20 -0300

    pagina we con FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,23 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse # para responder n HTML
 
-'''from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+app = FastAPI() # creo una instancia de la aplicación fastAPI
 
-app = FastAPI()
-
-@app.get('/')
-def get_list():
+@app.get('/')       # decorador
+def get_list():     # creo el recurso
     return [1,2,3,]
 
+'''
+@app.get('/contact')
+def get_list():
+    return {"name": "Platzi"}
+'''
+# Así responde FastAPI en HTML
 @app.get('/contact', response_class=HTMLResponse)
 def get_list():
     return """
         <h1>Hola soy una pagina</h1>
         <p>soy un parrafo</p>
     """
-
-'''
-
+    
 def run():
     store.get_categories()
 
--- web-server/store.py
+++ web-server/store.py
@@ -2,9 +2,14 @@ import requests
 
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    print(r.status_code)
-    print(r.text)
+    print(r.status_code) #Estado en HTTP
+    print(r.text)   # Información de la API (devuelve un string)
     print(type(r.text))
-    categories = r.json()
+
+    categories = r.json()   # En vez de preguntar por text pregunto por formato json.
+                            # Formato de lista de diccionarios que Python puede interpretar
+    
     for category in categories:
         print(category['name'])
+
+    
