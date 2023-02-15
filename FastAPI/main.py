0d4dff0cd688515525f04a4a758de1c7b2b415fa
Nico Duje <nicoduje@gmail.com>
2023-02-15 04:39:31 -0300

    Modifico el archivo main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI         #FastAPI trabaja sobre Pydantic
-from fastapi import Body
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -25,3 +25,32 @@ def home():         #Path function
 def create_person(person: Person = Body (...)): # EL "..." significa que ese parámetro es obligatorio
     return person
 
+#Validations: Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1,   #valido que el nombre tenga entre 1 y 50 caracteres
+        max_length=50,
+        title= 'Person Name',
+        description= 'This is the person name between 1 and 50 characters'
+        ), 
+    age: Optional[str] = Query(
+        ...,
+        title='Person Age',
+        description="This is the person age. It's requiered"
+        )
+):
+    return {name: age}
+
+
+# Validations: Path Parameters
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title=" Person ID",
+        description="It's the description of Person ID")
+):
+    return {person_id: 'It exists!'}
