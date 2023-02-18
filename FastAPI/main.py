ff2c751b64262712bb353636a56ed2321c262b04
Nico Duje <nicoduje@gmail.com>
2023-02-18 03:28:12 -0300

    Finalizo el archivo main del curso FastAPI

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -3,7 +3,7 @@ from typing import Optional
 from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from pydantic import Field  #Field igual a Body, Query y Path pero está directamente realcionada a Pydantic
 
 #FastAPI
@@ -29,21 +29,37 @@ class Person(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example= "Nicolas"
     )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example= "Duje Pereyra"
     )
     age: int = Field(
         ...,
         gt=1,
-        le=115
+        le=115,
+        example= 78
     )
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)
-    
+    hair_color: Optional[HairColor] = Field(default=None, example="black")
+    is_married: Optional[bool] = Field(default=None, example=False)
+    email: EmailStr = Field(..., example="nicoduje@gmail.com")
+
+    # Se crea una clase Config dentro de la clase Person para que ya tenga los datos cargados por defecto.
+    # Sirve para probar la API. Compento esta clase config porque coloco los examples en cada atributo de la clase person
+#    class Config:  
+#        schema_extra = {
+#            "example": {
+#                "first_name": "Nicolas",
+#                "last_name": "Duje",
+#                "age": 33,
+#                "hair_color": "black",
+#                "is_married": False
+#            }
+#        }
 
 #Path operation decorator. Utiliza el método get que viene de la instancia app.
 @app.get('/')
@@ -63,12 +79,14 @@ def show_person(
         min_length=1,   #valido que el nombre tenga entre 1 y 50 caracteres
         max_length=50,
         title= 'Person Name',
-        description= 'This is the person name between 1 and 50 characters'
+        description= 'This is the person name between 1 and 50 characters',
+        example= "Carlos"
         ), 
     age: Optional[str] = Query(
         ...,
         title='Person Age',
-        description="This is the person age. It's requiered"
+        description="This is the person age. It's requiered",
+        example= 34
         )
 ):
     return {name: age}
@@ -81,7 +99,9 @@ def show_person(
         ...,
         gt=0,
         title=" Person ID",
-        description="It's the description of Person ID")
+        description="It's the description of Person ID",
+        example= 123
+        )
 ):
     return {person_id: 'It exists!'}
 
